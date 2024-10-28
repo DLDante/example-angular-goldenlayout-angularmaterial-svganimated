382b53a3c42550882d22db537ee83e51122a59fd
DLDante <corvellio@gmail.com>
2024-10-28 19:14:38 -0300

    Moved card to make background color switch more noticeable

--- src/app/views/main-view/main-view.component.ts
+++ src/app/views/main-view/main-view.component.ts
@@ -1,53 +1,66 @@
-import { Component, OnInit, Inject, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Inject,
+  ChangeDetectorRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Container } from 'golden-layout';
 import GoldenLayout from 'golden-layout';
-import { SharedService } from "./../../shared.service";
-import {FormControl} from '@angular/forms';
+import { SharedService } from './../../shared.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
   encapsulation: ViewEncapsulation.None,
 })
-export class MainViewComponent implements OnInit {
+export class MainViewComponent implements OnInit, AfterViewInit {
   svgBgColorSelect: FormControl = new FormControl();
   svgBgColorSelected = 'white';
 
   constructor(
     //inject parent container
-    @Inject("Container") public container: Container,
-    @Inject("GoldenLayout") public layout: GoldenLayout,
+    @Inject('Container') public container: Container,
+    @Inject('GoldenLayout') public layout: GoldenLayout,
     private detector: ChangeDetectorRef,
     public service: SharedService
   ) {
-    this.svgBgColorSelect.valueChanges.subscribe((value) => this.svgBgColorSelected = value);
+    this.svgBgColorSelect.valueChanges.subscribe(
+      (value) => (this.svgBgColorSelected = value)
+    );
   }
 
   get width() {
-    return this.container.width
+    return this.container.width;
   }
   get height() {
-    return this.container.height
+    return this.container.height;
   }
-  set width(val:number) {
+  set width(val: number) {
     this.container.setSize(val, this.height);
   }
-  set height(val:number) {
+  set height(val: number) {
     this.container.setSize(this.width, val);
+  }
+
+  ngAfterViewInit() {
+    this.svgBgColorSelect.setValue('');
   }
 
   ngOnInit() {
     //here we can change container state
-    this.container.setTitle("MainView Title");
+    this.container.setTitle('MainView Title');
 
-    this.container.on("resize", _ => {
+    this.container.on('resize', (_) => {
       //we need to tell angular that something was changed
       this.detector.detectChanges();
-    })
+    });
 
-    console.log("main view initialized");
+    console.log('main view initialized');
   }
   ngOnDestroy(): void {
-    console.log("main view destroyed");
+    console.log('main view destroyed');
   }
 }
